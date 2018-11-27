75db9e063aee7986de8664b3aa881e4802a01b30
Cynthia Lee <leec@thoughtworks.com>
2018-11-27 14:59:37 +0000

    [Kavita & Cynthia] added lambda code to accept xml and edited python script to send xml to endpoint

--- lambda/main.js
+++ lambda/main.js
@@ -4,7 +4,7 @@ exports.handler = function (event, context, callback) {
     var response = {
         statusCode: 200,
         headers: {
-            'Content-Type': 'text/html; charset=utf-8',
+            'Content-Type': 'text/xml; charset=utf-8',
         },
         body: "<p>Hello world!</p>",
     };
--- scripts/sendMessage.py
+++ scripts/sendMessage.py
@@ -1,18 +1,12 @@
 import sys
 import requests
-import json
 
 filename = sys.argv[1]
-
-
 file = open(filename, 'r')
+
 messageContent = file.read()
-response = requests.get('https://qcq3n9xx8c.execute-api.eu-west-2.amazonaws.com/test', messageContent)
+headers = {'Content-Type': 'application/xml'}
+response = requests.post('https://qcq3n9xx8c.execute-api.eu-west-2.amazonaws.com/test', messageContent, headers).text
 
-print(response.status_code)
-
-# if response.status_code == 200:
-#     return json.loads(response.content.decode('utf-8'))
-# else:
-#     return None
+print(response)
 
