bca1659ace44e98c44e783c119adba1052ff65ec
Kavita Varsani <kavi.b0208@gmail.com>
2018-11-29 14:22:19 +0000

    change endpoint in sendMessage.py

--- scripts/sendMessage.py
+++ scripts/sendMessage.py
@@ -6,7 +6,7 @@ file = open(filename, 'r')
 
 messageContent = file.read()
 headers = {'Content-Type': 'application/xml'}
-response = requests.post('https://f7gy49bh6a.execute-api.eu-west-2.amazonaws.com/test', messageContent, headers)
+response = requests.post('https://r5w3yuys2l.execute-api.eu-west-2.amazonaws.com/test', messageContent, headers)
 
 print(response.status_code)
 print(response.text)
