143ec2d1029a5c92b4adc9fbfeadd8a926f2c182
jdunleav <jdunleav@thoughtworks.com>
2018-11-30 14:36:34 +0000

    Service Unavailable: Fixing permissions issue with s3 access

--- lambda.tf
+++ lambda.tf
@@ -293,6 +293,7 @@ resource "aws_iam_role_policy" "prm-infra-codepipeline-policy" {
     {
       "Effect":"Allow",
       "Action": [
+        "s3:PutObject",
         "s3:GetObject",
         "s3:GetObjectVersion",
         "s3:GetBucketVersioning"
