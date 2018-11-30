6f3be3cb27c1b32f89422ca23421290c90c97caf
jdunleav <jdunleav@thoughtworks.com>
2018-11-30 13:11:20 +0000

    Service Unavailable: Sample pipeline

--- lambda.tf
+++ lambda.tf
@@ -241,3 +241,130 @@ resource "aws_codebuild_project" "prm-vcs-trigger" {
     buildspec = "webhook_trigger.yml"
   }
 }
+
+resource "aws_codebuild_project" "prm-infra-validate" {
+  name = "prm-infra-validate"
+  description = "Validates the infrastructure"
+  build_timeout = "5"
+  service_role = "${aws_iam_role.codebuild-prm-trigger-service-role.arn}"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type = "S3"
+    location = "${aws_lambda_function.ehr_extract_handler.s3_bucket}"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/python:3.6.5"
+    type = "LINUX_CONTAINER"
+  }
+
+  source {
+    type = "CODEPIPELINE"
+  }
+}
+
+
+resource "aws_iam_role" "prm-infra-codepipeline" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "prm-infra-codepipeline-policy" {
+  name = "prm-infra-codepipeline-policy"
+  role = "${aws_iam_role.prm-infra-codepipeline.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.prm-infra-codepipeline-bucket.arn}",
+        "${aws_s3_bucket.prm-infra-codepipeline-bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "prm-infra-codepipeline-bucket" {
+  bucket = "prm-codepipeline-bucket"
+  acl    = "private"
+}
+
+resource "aws_codepipeline" "prm-infra-pipeline" {
+  name     = "prm-infra-pipeline"
+  role_arn = "${aws_iam_role.prm-infra-codepipeline.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.prm-infra-codepipeline-bucket.bucket}"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "S3"
+      version          = "1"
+
+      configuration {
+        S3Bucket  = "terraform-serverless-kc4"
+        S3ObjectKey   = "source"
+      }
+    }
+  }
+
+  stage {
+    name = "Validate"
+
+    action {
+      name            = "Validate"
+      category        = "Test"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+
+      configuration {
+        ProjectName = "${aws_codebuild_project.prm-infra-validate.name}"
+      }
+    }
+  }
+}
