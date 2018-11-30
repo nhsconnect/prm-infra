067ec8890b7d28894a5da97d890bf6837cb4a242
jdunleav <jdunleav@thoughtworks.com>
2018-11-30 19:19:40 +0000

    Service Unavailable: Added apply

--- lambda.tf
+++ lambda.tf
@@ -336,6 +336,82 @@ resource "aws_codebuild_project" "prm-infra-plan" {
   }
 }
 
+resource "aws_iam_role" "codebuild-prm-infra-apply-role" {
+  name = "codebuild-prm-infra-apply-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild-prm-infra-apply-service-policy" {
+  role = "${aws_iam_role.codebuild-prm-infra-apply-role.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.prm-infra-codepipeline-bucket.arn}",
+        "${aws_s3_bucket.prm-infra-codepipeline-bucket.arn}/*",
+        "arn:aws:s3:::my-terraform-state-kcj",
+        "arn:aws:s3:::my-terraform-state-kcj/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_codebuild_project" "prm-infra-apply" {
+  name = "prm-infra-apply"
+  description = "Applies the infrastructure"
+  build_timeout = "5"
+  service_role = "${aws_iam_role.codebuild-prm-infra-apply-role.arn}"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/python:3.6.5"
+    type = "LINUX_CONTAINER"
+  }
+
+  source {
+    type = "CODEPIPELINE"
+    buildspec = "infra_apply.yml"
+  }
+}
+
 resource "aws_iam_role" "prm-infra-codepipeline" {
   name = "test-role"
 
@@ -439,4 +515,21 @@ resource "aws_codepipeline" "prm-infra-pipeline" {
       }
     }
   }
+
+  stage {
+    name = "Apply"
+
+    action {
+      name            = "Apply"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+      input_artifacts = ["source"]
+ 
+      configuration {
+        ProjectName = "${aws_codebuild_project.prm-infra-apply.name}"
+      }
+    }
+  }  
 }
