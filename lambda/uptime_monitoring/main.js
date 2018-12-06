b44d7562eb1c94519f021a3240fcbe0fc2e87db7
Cynthia Lee <leec@thoughtworks.com>
2018-12-06 14:35:07 +0000

    PRM-10: Edit UptimeMonitoring main.js to make two endpoint calls, one for 5xx and one for 4xx

--- lambda/uptime_monitoring/main.js
+++ lambda/uptime_monitoring/main.js
@@ -3,12 +3,12 @@ const URL = require('url');
 
 exports.handler = async (event, context) => {
     const myURL = new URL.URL(process.env.url);
+    const path = process.env.stage;
 
-    return new Promise((resolve, reject) => {
+    const response1 = new Promise((resolve, reject) => {
         const options = {
             host: myURL.host,
-            path: '/test',
-            // port: 8000,
+            path: "/test",
             method: 'POST'
         };
 
@@ -24,4 +24,26 @@ exports.handler = async (event, context) => {
         req.write('');
         req.end();
     });
+
+    const response2 = new Promise((resolve, reject) => {
+        const options = {
+            host: myURL.host,
+            path: "/abc",
+            method: 'POST'
+        };
+
+        const req = http.request(options, (res) => {
+            console.log(`res ${res.statusCode}`);
+            resolve('Success');
+        });
+
+        req.on('error', (e) => {
+            reject(e.message);
+        });
+
+        req.write('');
+        req.end();
+    });
+
+    return Promise.all([response1, response2]);
 };
