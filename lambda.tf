604e9a09528bc6010dda33011bd49ef285221b62
jdunleav <jdunleav@thoughtworks.com>
2018-12-06 14:34:54 +0000

    PRM-10: Added infrastructure validation step to pipeline

--- lambda.tf
+++ lambda.tf
@@ -592,6 +592,88 @@ resource "aws_codebuild_project" "prm-infra-apply" {
   }
 }
 
+resource "aws_iam_role" "codebuild-prm-infra-validate-role" {
+  name = "codebuild-prm-infra-valdiate-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild-prm-infra-validate-service-policy" {
+  role = "${aws_iam_role.codebuild-prm-infra-validate-role.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "lambda:CreateFunction",
+        "lambda:UpdateEventSourceMapping",
+        "lambda:ListFunctions",
+        "apigateway:*",
+        "s3:*",
+        "lambda:GetEventSourceMapping",
+        "logs:*",
+        "lambda:GetAccountSettings",
+        "lambda:CreateEventSourceMapping",
+        "codebuild:*",
+        "iam:*",
+        "cloudwatch:*",
+        "kms:*",
+        "ssm:*",
+        "codedeploy:*",
+        "lambda:*",
+        "lambda:ListEventSourceMappings",
+        "ec2:*",
+        "codepipeline:*",
+        "lambda:DeleteEventSourceMapping",
+        "events:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+
+
+
+resource "aws_codebuild_project" "prm-infra-validate" {
+  name = "prm-infra-validate"
+  description = "Validates the infrastructure"
+  build_timeout = "5"
+  service_role = "${aws_iam_role.codebuild-prm-infra-validate-role.arn}"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/python:3.6.5"
+    type = "LINUX_CONTAINER"
+  }
+
+  source {
+    type = "CODEPIPELINE"
+    buildspec = "infra_validate.yml"
+  }
+}
+
 resource "aws_iam_role" "prm-infra-codepipeline" {
   name = "test-role"
 
@@ -756,4 +838,21 @@ resource "aws_codepipeline" "prm-infra-pipeline" {
       }
     }
   }
+
+  stage {
+    name = "Validate"
+
+    action {
+      name            = "Validate"
+      category        = "Test"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+      input_artifacts = ["source"]
+
+      configuration {
+        ProjectName = "${aws_codebuild_project.prm-infra-validate.name}"
+      }
+    }
+  }  
 }
