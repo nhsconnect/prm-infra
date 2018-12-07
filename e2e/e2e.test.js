03d5904e41919a6bc8ae62f8963ba21f599cdc23
jdunleav <jdunleav@thoughtworks.com>
2018-12-07 16:38:48 +0000

    PRM-10: Enabled correct checking of status code in test

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,22 +1,22 @@
 const Url = require("url");
 const request = require("request-promise-native");
-const errors = require('request-promise-native/errors');
+const errors = require("request-promise-native/errors");
 
 const PRM_URL = new Url.URL(process.env.PRM_ENDPOINT);
 
-console.log(PRM_URL)
-
-test('That PRM tells us when we use the wrong endpoint', async () => {
-    expect.assertions(2);
-    try {
-        await request.post(`${PRM_URL.origin}/bleh`);
-    } catch (e) {
-        expect(e.statusCode).toBe(403);
-        expect(e instanceof errors.StatusCodeError).toBeTruthy();
-    }
+test("That PRM tells us when we use the wrong endpoint", async () => {
+  expect.assertions(2);
+  try {
+    await request.post(`${PRM_URL.origin}/bleh`);
+  } catch (e) {
+    expect(e.statusCode).toBe(403);
+    expect(e instanceof errors.StatusCodeError).toBeTruthy();
+  }
 });
 
-test('That PRM tells us that it is, broadly speaking, working', async () => {
-    const response = await request.post(`${PRM_URL.origin}/test`);
-    expect(response.statusCode).toBe(200);
+test("That PRM tells us that it is, broadly speaking, working", async () => {
+  const response = await request.post(`${PRM_URL.origin}/test`, {
+    resolveWithFullResponse: true
+  });
+  expect(response.statusCode).toBe(200);
 });
