69b13cd63a1fe6dc62fdfbf8d9005f6907b411bc
Cynthia Lee <leec@thoughtworks.com>
2018-12-07 09:41:56 +0000

    PRM-10: Comment out alarm settings

--- lambda.tf
+++ lambda.tf
@@ -47,7 +47,7 @@ resource "aws_api_gateway_method" "proxy" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_settings" "s" {
+resource "aws_api_gateway_method_settings" "api_gw_method_settings" {
   rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   stage_name  = "test"
   method_path = "*/*"
@@ -134,40 +134,40 @@ resource "aws_iam_role" "lambda_exec" {
 }
 EOF
 }
-
-resource "aws_cloudwatch_metric_alarm" "notify-error-4xx" {
-  alarm_name = "terraform-test-notify-error-4xx"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  metric_name = "4XXError"
-  namespace = "AWS/ApiGateway"
-  period = "300"
-  statistic = "Sum"
-  threshold = "0"
-  alarm_description = "This metric monitors 4xx errors on API-gateway"
-  insufficient_data_actions = []
-
-  dimensions {
-    ApiName = "${aws_api_gateway_rest_api.ehr_extract_handler_api.name}"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "notify-error-5xx" {
-  alarm_name = "terraform-test-notify-error-5xx"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  metric_name = "5XXError"
-  namespace = "AWS/ApiGateway"
-  period = "300"
-  statistic = "Sum"
-  threshold = "0"
-  alarm_description = "This metric monitors 5xx errors on API-gateway"
-  insufficient_data_actions = []
-
-  dimensions {
-    ApiName = "${aws_api_gateway_rest_api.ehr_extract_handler_api.name}"
-  }
-}
+//
+//resource "aws_cloudwatch_metric_alarm" "notify-error-4xx" {
+//  alarm_name = "terraform-test-notify-error-4xx"
+//  comparison_operator = "GreaterThanThreshold"
+//  evaluation_periods = "1"
+//  metric_name = "4XXError"
+//  namespace = "AWS/ApiGateway"
+//  period = "300"
+//  statistic = "Sum"
+//  threshold = "0"
+//  alarm_description = "This metric monitors 4xx errors on API-gateway"
+//  insufficient_data_actions = []
+//
+//  dimensions {
+//    ApiName = "${aws_api_gateway_rest_api.ehr_extract_handler_api.name}"
+//  }
+//}
+//
+//resource "aws_cloudwatch_metric_alarm" "notify-error-5xx" {
+//  alarm_name = "terraform-test-notify-error-5xx"
+//  comparison_operator = "GreaterThanThreshold"
+//  evaluation_periods = "1"
+//  metric_name = "5XXError"
+//  namespace = "AWS/ApiGateway"
+//  period = "300"
+//  statistic = "Sum"
+//  threshold = "0"
+//  alarm_description = "This metric monitors 5xx errors on API-gateway"
+//  insufficient_data_actions = []
+//
+//  dimensions {
+//    ApiName = "${aws_api_gateway_rest_api.ehr_extract_handler_api.name}"
+//  }
+//}
 
 resource "aws_cloudwatch_event_rule" "every_three_mins_rule" {
   name = "every-minute"
