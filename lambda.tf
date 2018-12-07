c3dc6db3e1d313ffdad476b5ef60a765447b2f87
Cynthia Lee <leec@thoughtworks.com>
2018-12-07 11:53:19 +0000

    PRM-10: Separate out Cloudwatch metric alarms into separate file

--- lambda.tf
+++ lambda.tf
@@ -49,52 +49,6 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
-resource "aws_cloudwatch_metric_alarm" "notify-error-4xx" {
-  alarm_name = "terraform-test-notify-error-4xx"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  metric_name = "4XXError"
-  namespace = "AWS/ApiGateway"
-  period = "300"
-  statistic = "Sum"
-  threshold = "0"
-  alarm_description = "This metric monitors 4xx errors on API-gateway"
-  insufficient_data_actions = []
-
-  dimensions {
-    ApiName = "${aws_api_gateway_rest_api.ehr_extract_handler_api.name}"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "notify-error-5xx" {
-  alarm_name = "terraform-test-notify-error-5xx"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods = "1"
-  metric_name = "5XXError"
-  namespace = "AWS/ApiGateway"
-  period = "300"
-  statistic = "Sum"
-  threshold = "0"
-  alarm_description = "This metric monitors 5xx errors on API-gateway"
-  insufficient_data_actions = []
-
-  dimensions {
-    ApiName = "${aws_api_gateway_rest_api.ehr_extract_handler_api.name}"
-  }
-}
-
-resource "aws_cloudwatch_event_rule" "every_three_mins_rule" {
-  name = "every-minute"
-  description = "Fires every three minutes"
-  schedule_expression = "rate(1 minute)"
-}
-
-resource "aws_cloudwatch_event_target" "every_three_minutes_event_target" {
-  rule = "${aws_cloudwatch_event_rule.every_three_mins_rule.name}"
-//  target_id = "check_pinger"
-  arn = "${aws_lambda_function.uptime_monitoring.arn}"
-}
-
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_uptime_monitoring_lambda" {
   statement_id = "AllowExecutionFromCloudWatch"
   action = "lambda:InvokeFunction"
