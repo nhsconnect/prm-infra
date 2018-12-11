77e32f5f6bba0bb4bc495669bf65772521ead04a
Cynthia Lee <leec@thoughtworks.com>
2018-12-11 13:23:33 +0000

    PRM-4: Add roles and policies for lambdas into lambdas pipeline

--- lambda-pipeline.tf
+++ lambda-pipeline.tf
@@ -89,6 +89,8 @@ resource "aws_iam_role_policy" "prm-lambda-codepipeline-policy" {
         "s3:*"
       ],
       "Resource": [
+        "${aws_s3_bucket.prm-infra-codepipeline-bucket.arn}",
+        "${aws_s3_bucket.prm-infra-codepipeline-bucket.arn}/*",
         "arn:aws:s3:::${var.prm-application-source-bucket}",
         "arn:aws:s3:::${var.prm-application-source-bucket}/*"
       ]
@@ -148,4 +150,120 @@ resource "aws_codebuild_project" "prm-build-ehr-extract-lambda" {
     type      = "CODEPIPELINE"
     buildspec = "ehr_extract_handler.yml"
   }
+}
+
+resource "aws_iam_role" "codebuild-prm-ehr-extract-lambda-role" {
+  name = "codebuild-prm-ehr-extract-lambda-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild-prm-ehr-extract-lambda-policy" {
+  role = "${aws_iam_role.codebuild-prm-ehr-extract-lambda-role.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "lambda:CreateFunction",
+        "lambda:UpdateEventSourceMapping",
+        "lambda:ListFunctions",
+        "apigateway:*",
+        "s3:*",
+        "lambda:GetEventSourceMapping",
+        "logs:*",
+        "lambda:GetAccountSettings",
+        "lambda:CreateEventSourceMapping",
+        "codebuild:*",
+        "iam:*",
+        "cloudwatch:*",
+        "kms:*",
+        "ssm:*",
+        "codedeploy:*",
+        "lambda:*",
+        "lambda:ListEventSourceMappings",
+        "ec2:*",
+        "codepipeline:*",
+        "lambda:DeleteEventSourceMapping",
+        "events:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role" "codebuild-prm-uptime-monitoring-lambda-role" {
+  name = "codebuild-prm-uptime-monitoring-lambda-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild-prm-uptime-monitoring-lambda-policy" {
+  role = "${aws_iam_role.codebuild-prm-uptime-monitoring-lambda-role.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "lambda:CreateFunction",
+        "lambda:UpdateEventSourceMapping",
+        "lambda:ListFunctions",
+        "apigateway:*",
+        "s3:*",
+        "lambda:GetEventSourceMapping",
+        "logs:*",
+        "lambda:GetAccountSettings",
+        "lambda:CreateEventSourceMapping",
+        "codebuild:*",
+        "iam:*",
+        "cloudwatch:*",
+        "kms:*",
+        "ssm:*",
+        "codedeploy:*",
+        "lambda:*",
+        "lambda:ListEventSourceMappings",
+        "ec2:*",
+        "codepipeline:*",
+        "lambda:DeleteEventSourceMapping",
+        "events:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+POLICY
 }
