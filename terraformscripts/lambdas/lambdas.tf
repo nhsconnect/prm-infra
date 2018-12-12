0bf276acac9f6cb07a6474a1423d49030f41b0ff
Vincenzo Fabrizi <vfabrizi@thoughtworks.com>
2018-12-12 14:25:31 +0000

    PRM-4: cleaned up the lambdas remote state

--- terraformscripts/lambdas/lambdas.tf
+++ terraformscripts/lambdas/lambdas.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "ehr_extract_handler" {
 
   //  s3_bucket     = "${var.prm-application-source-bucket}"
   //  s3_key        = "example.zip"
-  filename = "${path.root}/example.zip"
+  filename = "${path.root}/dummy_ehr_extract_handler.zip"
 
   handler = "main.handler"
   runtime = "nodejs8.10"
@@ -15,9 +15,9 @@ resource "aws_lambda_function" "uptime_monitoring" {
 
   //  s3_bucket     = "${aws_s3_bucket.uptime_monitoring_bucket.bucket}"
   //  s3_key        = "uptime_monitoring.zip"
-  filename = "${path.root}/example.zip"
+  filename = "${path.root}/dummy_uptime_monitoring.zip"
 
-  //  source_code_hash = "${base64sha256(format("%s/example.zip", path.root))}"
+  //  source_code_hash = "${base64sha256(format("%s/dummy_uptime_monitoring.zip", path.root))}"
   handler = "main.handler"
   runtime = "nodejs8.10"
   role    = "${aws_iam_role.lambda_exec.arn}"
