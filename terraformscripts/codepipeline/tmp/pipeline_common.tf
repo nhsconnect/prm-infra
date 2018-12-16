299a47d538e04fcd9de2040c931f64aca2430fbc
Vincenzo Fabrizi <vfabrizi@thoughtworks.com>
2018-12-16 01:53:11 +0000

    PRM-23: Codepipeline code refactor, push code to Codepipeline utility

--- terraformscripts/codepipeline/tmp/pipeline_common.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "prm-codebuild-artifact" {
-  bucket        = "prm-codebuild-artifact"
-  acl           = "private"
-  force_destroy = true
-
-  versioning {
-    enabled = true
-  }
-}
