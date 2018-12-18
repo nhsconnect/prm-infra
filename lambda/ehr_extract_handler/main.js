28e1bec7b996f124ca25d265f8f6a35b13abb3eb
Kavita Varsani <kavi.b0208@gmail.com>
2018-12-18 10:03:13 +0000

    PRM-30: update EHR_extract_handler code to contain code that adds to dynamo db

--- lambda/ehr_extract_handler/main.js
+++ lambda/ehr_extract_handler/main.js
@@ -1,12 +1,59 @@
-'use strict';
+// 'use strict';
 
-exports.handler = function (event, context, callback) {
-    var response = {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'text/xml; charset=utf-8',
-        },
-        body: "Hey Vini!",
+// exports.handler = function (event, context, callback) {
+//     var response = {
+//         statusCode: 200,
+//         headers: {
+//             'Content-Type': 'text/xml; charset=utf-8',
+//         },
+//         body: "Hey Vini!",
+//     };
+//     callback(null, response);
+// };
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create DynamoDB service object
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+var params = {
+    RequestItems: {
+        "PROCESS_STORAGE": [
+            {
+                PutRequest: {
+                    Item: {
+                        "KEY": { "PROCESS_ID": "PROCESS_PAYLOAD" },
+                        1234: "PROCESSING"
+                    }
+                }
+            }
+        ]
+    }
+};
+
+exports.handler = async (event) => {
+    ddb.batchWriteItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
+
+    const responseBody = {
+        "uuid": "12344"
     };
-    callback(null, response);
+
+    const response = {
+        "statusCode": 200,
+        "headers": {
+            "my_header": "my_value"
+        },
+        "body": JSON.stringify(responseBody),
+        "isBase64Encoded": false
+    };
+    return response;
 };
