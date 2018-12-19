53074d5d37c76beffa19074d488fc5c337c0f310
Kavita Varsani <kavi.b0208@gmail.com>
2018-12-19 15:13:48 +0000

    PRM-30: refactor api gateway end points

--- terraformscripts/lambdas/api_gateway.tf
+++ terraformscripts/lambdas/api_gateway.tf
@@ -11,19 +11,6 @@ resource "aws_api_gateway_account" "demo" {
   cloudwatch_role_arn = "${aws_iam_role.cloudwatch-apigateway-log-role.arn}"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-  parent_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_method_settings" "api_gw_method_settings" {
   rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   stage_name  = "${aws_api_gateway_deployment.api_gw_deployment.stage_name}"
@@ -35,43 +22,57 @@ resource "aws_api_gateway_method_settings" "api_gw_method_settings" {
   }
 }
 
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
-}
-
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-  resource_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
-}
-
 resource "aws_api_gateway_deployment" "api_gw_deployment" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.send_integration"
+    //    "aws_api_gateway_integration.lambda",
+    //    "aws_api_gateway_integration.lambda_root",
   ]
-
   rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   stage_name  = "dev"
 }
 
+//resource "aws_api_gateway_resource" "proxy" {
+//  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
+//  parent_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
+//  path_part   = "{proxy+}"
+//}
+//
+//resource "aws_api_gateway_method" "proxy" {
+//  rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
+//  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+//  http_method   = "POST"
+//  authorization = "NONE"
+//}
+
+//resource "aws_api_gateway_integration" "lambda" {
+//  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
+//  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+//  http_method = "${aws_api_gateway_method.proxy.http_method}"
+//
+//  integration_http_method = "POST"
+//  type                    = "AWS_PROXY"
+//  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
+//}
+
+//resource "aws_api_gateway_method" "proxy_root" {
+//  rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
+//  resource_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
+//  http_method   = "ANY"
+//  authorization = "NONE"
+//}
+
+//resource "aws_api_gateway_integration" "lambda_root" {
+//  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
+//  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+//  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+//
+//  integration_http_method = "POST"
+//  type                    = "AWS_PROXY"
+//  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
+//}
+
+# /send
 resource "aws_api_gateway_resource" "send" {
   rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   parent_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
@@ -92,7 +93,7 @@ resource "aws_api_gateway_integration" "send_integration" {
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
+  uri                     = "arn:aws:apigateway:eu-west-2:lambda:path/2015-03-31/functions/${aws_lambda_function.ehr_extract_handler.arn}/invocations"
 }
 
 # /status
