4e73061005ec703de1cc6bc3272029cd9c3ac53d
Vincenzo Fabrizi <vfabrizi@thoughtworks.com>
2018-12-21 12:42:21 +0000

    PRM-04: Uniquely identifiable artifacts bucket

--- terraformscripts/tf/codepipeline/pipeline_common.tf
+++ terraformscripts/tf/codepipeline/pipeline_common.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "prm-codebuild-artifact" {
-  bucket        = "prm-codebuild-artifact"
+  bucket        = "prm-${data.aws_caller_identity.current.account_id}-codebuild-artifact"
   acl           = "private"
   force_destroy = true
 
