9a91e6a93a15936fc471cec58f65e8a63ac738f6
cynthiatw <leec@thoughtworks.com>
2019-01-03 14:52:14 +0000

    PRM-30: Change HTTP method in method integration resource for /status{uuid} to POST

--- terraformscripts/tf/lambdas/api_gateway.tf
+++ terraformscripts/tf/lambdas/api_gateway.tf
@@ -114,8 +114,7 @@ resource "aws_api_gateway_resource" "status" {
 resource "aws_api_gateway_method" "status_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   resource_id   = "${aws_api_gateway_resource.status.id}"
-  http_method   = "GET"
-//  http_method = "POST"
+  http_method = "POST"
   authorization = "NONE"
   
   request_parameters {
@@ -128,7 +127,7 @@ resource "aws_api_gateway_integration" "status_integration" {
   resource_id = "${aws_api_gateway_method.status_method.resource_id}"
   http_method = "${aws_api_gateway_method.status_method.http_method}"
 
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:eu-west-2:lambda:path/2015-03-31/functions/${aws_lambda_function.retrieve_status.arn}/invocations"
 }
