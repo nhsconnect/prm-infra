0409d495512a69f3da083ee5c0556cd3ee9749c3
cynthiatw <leec@thoughtworks.com>
2019-01-04 15:51:58 +0000

    PRM-30: Add a build step to run retrieve_processed_ehr_extract/build_spec.yml

--- terraformscripts/tf/lambdas/api_gateway.tf
+++ terraformscripts/tf/lambdas/api_gateway.tf
@@ -26,52 +26,9 @@ resource "aws_api_gateway_deployment" "api_gw_deployment" {
     "aws_api_gateway_integration.send_integration",
   ]
 
-  //    "aws_api_gateway_integration.lambda",
-  //    "aws_api_gateway_integration.lambda_root",
-
   rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   stage_name  = "dev"
 }
-
-//resource "aws_api_gateway_resource" "proxy" {
-//  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-//  parent_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
-//  path_part   = "{proxy+}"
-//}
-//
-//resource "aws_api_gateway_method" "proxy" {
-//  rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-//  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-//  http_method   = "POST"
-//  authorization = "NONE"
-//}
-
-//resource "aws_api_gateway_integration" "lambda" {
-//  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-//  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-//  http_method = "${aws_api_gateway_method.proxy.http_method}"
-//
-//  integration_http_method = "POST"
-//  type                    = "AWS_PROXY"
-//  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
-//}
-
-//resource "aws_api_gateway_method" "proxy_root" {
-//  rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-//  resource_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.root_resource_id}"
-//  http_method   = "ANY"
-//  authorization = "NONE"
-//}
-
-//resource "aws_api_gateway_integration" "lambda_root" {
-//  rest_api_id = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
-//  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
-//  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
-//
-//  integration_http_method = "POST"
-//  type                    = "AWS_PROXY"
-//  uri                     = "${aws_lambda_function.ehr_extract_handler.invoke_arn}"
-//}
 
 # /send
 resource "aws_api_gateway_resource" "send" {
