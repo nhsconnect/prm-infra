80895d8381a881ac0ab884219dfd8f6f73d8daa4
Kavita Varsani <kavi.b0208@gmail.com>
2019-01-07 11:51:36 +0000

    PRM-30: changed method of /status to GET from POST

--- terraformscripts/tf/lambdas/api_gateway.tf
+++ terraformscripts/tf/lambdas/api_gateway.tf
@@ -70,7 +70,7 @@ resource "aws_api_gateway_resource" "status" {
 resource "aws_api_gateway_method" "status_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.ehr_extract_handler_api.id}"
   resource_id   = "${aws_api_gateway_resource.status.id}"
-  http_method = "POST"
+  http_method = "GET"
   authorization = "NONE"
   
   request_parameters {
