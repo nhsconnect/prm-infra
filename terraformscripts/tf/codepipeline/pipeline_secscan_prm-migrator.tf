83d46e36180b6310416cfcb61b6eee9965d098a3
Vincenzo Fabrizi <vfabrizi@thoughtworks.com>
2019-01-09 20:47:54 +0000

    PRM-44: adding yelp detect-secrets

--- terraformscripts/tf/codepipeline/pipeline_secscan_prm-migrator.tf
+++ terraformscripts/tf/codepipeline/pipeline_secscan_prm-migrator.tf
@@ -45,4 +45,22 @@ resource "aws_codepipeline" "prm-secscan-prm-migrator-pipeline" {
       }
     }
   }
+
+  stage {
+    name = "Scan_prm-migrator_repo_java"
+
+    action {
+      name            = "Plan"
+      category        = "Test"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+      input_artifacts = ["source"]
+      run_order       = 1
+
+      configuration {
+        ProjectName = "${aws_codebuild_project.prm-secscan-prm-migrator-scanjava.name}"
+      }
+    }
+  }
 }
--- terraformscripts/tf/codepipeline/project_secscan_prm-migrator.tf
+++ terraformscripts/tf/codepipeline/project_secscan_prm-migrator.tf
@@ -1,4 +1,4 @@
-# secscan prm-infra projects
+# secscan prm-migrator projects
 
 resource "aws_codebuild_project" "prm-secscan-prm-migrator-scan" {
   name          = "prm-secscan-prm-migrator-plan"
@@ -13,13 +13,35 @@ resource "aws_codebuild_project" "prm-secscan-prm-migrator-scan" {
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
 
-    #image        = "aws/codebuild/python:3.6.5"
-    image = "aws/codebuild/java:openjdk-9"
+    image = "aws/codebuild/python:3.6.5"
     type  = "LINUX_CONTAINER"
   }
 
   source {
     type      = "CODEPIPELINE"
     buildspec = "./pipeline_definition/secscan_prm-migrator_scan.yml"
+  }
+}
+
+resource "aws_codebuild_project" "prm-secscan-prm-migrator-scanjava" {
+  name          = "prm-secscan-prm-migrator-plan"
+  description   = "Scan the prm-infra repo"
+  build_timeout = "5"
+  service_role  = "${aws_iam_role.codebuild-project-generic-role.arn}"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+
+    image = "aws/codebuild/java:openjdk-9"
+    type  = "LINUX_CONTAINER"
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "./pipeline_definition/secscan_prm-migrator_scanjava.yml"
   }
 }
