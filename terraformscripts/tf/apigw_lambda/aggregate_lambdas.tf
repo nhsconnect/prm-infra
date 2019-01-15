f71138ddba363020df4f38eb41b2b841215dd8d9
Kavita Varsani <kavi.b0208@gmail.com>
2019-01-15 17:53:55 +0000

    PRM-30: name change for lambda

--- terraformscripts/tf/apigw_lambda/aggregate_lambdas.tf
+++ terraformscripts/tf/apigw_lambda/aggregate_lambdas.tf
@@ -2,7 +2,7 @@ module "apigw_lambda_ehr_extract_handler" {
   source      = "../modules/lambda/"
   aws_region  = "${var.aws_region}"
   environment = "${var.environment}"
-  lambda_name = "EhrExtractHandler"
+  lambda_name = "EhrExtractHandler----s"
   #vpc_id      = "${var.vpc_id}"
   #vpc_cidr    = "${var.vpc_cidr}"
 }
