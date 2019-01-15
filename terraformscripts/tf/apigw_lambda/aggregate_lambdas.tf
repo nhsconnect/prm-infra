ed1082ec65815a8814bc8b66ca2f5b8034ab7d3a
Kavita Varsani <kavi.b0208@gmail.com>
2019-01-15 16:46:09 +0000

    PRM-30: testing the terraform resource that creates a table in DynamoDB

--- terraformscripts/tf/apigw_lambda/aggregate_lambdas.tf
+++ terraformscripts/tf/apigw_lambda/aggregate_lambdas.tf
@@ -49,3 +49,19 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     enabled = true
   }
 }
+resource "aws_dynamodb_table" "test" {
+  name           = "THUNDERBIRD"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "PROCESS_ID"
+
+ attribute = {
+    name = "PROCESS_ID"
+    type = "S"
+  }
+  
+  server_side_encryption {
+    enabled = true
+  }
+}
