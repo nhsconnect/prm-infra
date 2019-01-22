d53c09e6a5dda5235e2c2ab170aeb439694666c8
jdunleav <jdunleav@thoughtworks.com>
2019-01-22 10:37:49 +0000

    PRM-21: Re-enabled lifecycle change tracking so that OAuthTokens are not a source of change as Terraform currently can't cope.

--- terraformscripts/tf/codepipeline/pipeline_lambdas.tf
+++ terraformscripts/tf/codepipeline/pipeline_lambdas.tf
@@ -1,5 +1,10 @@
 resource "aws_codepipeline" "lambda-pipeline" {
-  #lifecycle {  #  ignore_changes = [  #    "stage.0.action.0.configuration.OAuthToken",  #    "stage.0.action.0.configuration.%",  #  ]  #}
+  lifecycle {  
+   ignore_changes = [  
+     "stage.0.action.0.configuration.OAuthToken",  
+     "stage.0.action.0.configuration.%",  
+   ]  
+  }
 
   name     = "prm-lambda-pipeline"
   role_arn = "${aws_iam_role.codepipeline-generic-role.arn}"
