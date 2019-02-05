2e9408bf074197c16e94fe3c371f1eae25ed1d0a
Dale Peakall <dale@peakall.net>
2019-02-05 16:26:51 +0000

    PRM-92: modified test so that it behaves properly and increased timeout as PDS is slow

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,5 +1,4 @@
 const request = require("request-promise-native");
-const errors = require("request-promise-native/errors");
 const fs = require("fs")
 const path = require("path")
 
@@ -7,9 +6,9 @@ const PRIVATE_KEY_DATA = process.env.PRIVATE_KEY_DATA;
 const CERT_DATA = fs.readFileSync(path.resolve(__dirname, "cert.pem"))
 const REQUEST_DATA = fs.readFileSync(path.resolve(__dirname, "test-request.xml"))
 
-test("That PDS responds to a valid request", () => {
-  expect.assertions(1);  
+jest.setTimeout(30000);
 
+test("That PDS responds to a valid request", async () => {
   const options = {
     method: 'POST',
     url: 'https://192.168.128.11/smsp/pds',
@@ -20,13 +19,8 @@ test("That PDS responds to a valid request", () => {
       "Content-Type": "text/xml",
       "SOAPAction": "urn:nhs-itk:services:201005:getNHSNumber-v1-0"
     },
-    timeout: 2000
+    timeout: 20000
   };
 
-  request(options)
-    .then(function (body) {
-      expect(body.statusCode).toBe(200);
-    }).catch(function (err) {
-      console.log(err.message);
-    });
+  await request(options)
 });
