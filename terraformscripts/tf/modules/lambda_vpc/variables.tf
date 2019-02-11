7e6056a5f58904de6e4aaf0cd44e474b4856a96a
Dale Peakall <dale@peakall.net>
2019-02-11 17:08:43 +0000

    PRM-101: Remove duplicated variables from lambda_vpc

--- terraformscripts/tf/modules/lambda_vpc/variables.tf
+++ terraformscripts/tf/modules/lambda_vpc/variables.tf
@@ -24,27 +24,6 @@ variable "vpc_id" {
 
 variable "vpc_cidr" {
   default = "172.31.0.0/16"
-  default = "dev"
-}
-
-variable "lambda_name" {
-  default = "LambdaName"
-}
-
-variable "lambda_runtime" {
-  default = "nodejs8.10"
-}
-
-variable "lambda_handler" {
-  default = "main.handler"
-}
-
-variable "vpc_id" {
-  default = "vpc-8f3aaae7"
-}
-
-variable "vpc_cidr" {
-  default = "172.31.0.0/16"
 }
 
 variable "private_subnet_ids" {
