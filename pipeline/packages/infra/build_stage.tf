3cc9826bb7deb9e4e4eee40b2db234fd5da3592a
Dale Peakall <dale@peakall.net>
2019-02-13 16:02:48 +0000

    PRM-36: set executable bit on bouncerw and update to use 0.2.0 release of TF Docker image

--- pipeline/packages/infra/build_stage.tf
+++ pipeline/packages/infra/build_stage.tf
@@ -67,7 +67,7 @@ resource "aws_codebuild_project" "build" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "431593652018.dkr.ecr.eu-west-2.amazonaws.com/codebuild/terraform:0.1.0"
+    image        = "431593652018.dkr.ecr.eu-west-2.amazonaws.com/codebuild/terraform:0.2.0"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
