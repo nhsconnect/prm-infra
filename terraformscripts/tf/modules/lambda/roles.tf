62f9ae2b50bbcac2fd32714767e160b8e4f98803
Christian Taylor <ctaylor@thoughtworks.com>
2019-02-18 10:50:33 +0000

    Revert "PRM-101: add ssm get parameter permission to lambda module"
    This reverts commit 672da1c962e766c0d9108aef02ef1b5392cbd5f9.

--- terraformscripts/tf/modules/lambda/roles.tf
+++ terraformscripts/tf/modules/lambda/roles.tf
@@ -4,13 +4,7 @@ data "template_file" "handler_role" {
 
 data "template_file" "handler_policy" {
   template = "${file("${path.module}/iam_role_policy_lambda.json")}"
-  vars = {
-    AWS_REGION = "${var.aws_region}"
-    AWS_ACCOUNT_ID = "${data.aws_caller_identity.current.account_id}"
-  }
 }
-
-data "aws_caller_identity" "current" { }
 
 resource "aws_iam_role_policy" "handler_policy" {
   name   = "${var.environment}-${var.lambda_name}"
