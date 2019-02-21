304475fb98491b5dc884c976643b9d76efebc201
Kavita Varsani <kavi.b0208@gmail.com>
2019-02-21 15:40:44 +0000

    PRMT-81: remove scans from codepipeline, serving infra and lambda, comment out lifecyle

--- terraformscripts/tf/codepipeline/pipeline_servinginfra.tf
+++ terraformscripts/tf/codepipeline/pipeline_servinginfra.tf
@@ -1,10 +1,10 @@
 resource "aws_codepipeline" "prm-servinginfra-pipeline" {
-  lifecycle {
-    ignore_changes = [
-      "stage.0.action.0.configuration.OAuthToken",
-      "stage.0.action.0.configuration.%",
-    ]
-  }
+  # lifecycle {
+  #   ignore_changes = [
+  #     "stage.0.action.0.configuration.OAuthToken",
+  #     "stage.0.action.0.configuration.%",
+  #   ]
+  # }
 
   name     = "prm-servinginfra-pipeline"
   role_arn = "${aws_iam_role.codepipeline-generic-role.arn}"
