e5bc13b6a4be4f5697eadef77dc6752324b21a8f
Kavita Varsani <kavi.b0208@gmail.com>
2019-02-22 10:49:51 +0000

    PRMT-81: add cloudwatch rule to prm_infra_sec_scan_pipeline

--- terraformscripts/tf/codepipeline/pipeline_prm_infra_sec_scan.tf
+++ terraformscripts/tf/codepipeline/pipeline_prm_infra_sec_scan.tf
@@ -53,3 +53,13 @@ resource "aws_codepipeline" "prm-infra-sec-scan" {
     }
   }
 }
+resource "aws_cloudwatch_event_rule" "every_five_minutes_rule" {
+  name                = "every-five-minutes"
+  description         = "Fires every five minutes"
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "every_five_minutes_event_target" {
+  rule = "${aws_cloudwatch_event_rule.every_five_minutes_rule.name}"
+  arn  = "${aws_codepipeline.prm-infra-sec-scan.arn}"
+}
