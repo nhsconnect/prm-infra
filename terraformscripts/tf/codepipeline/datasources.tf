e4d70fdc7a8cc3bdd09cb8e79a7dcfae0bb57e69
Dale Peakall <dale@peakall.net>
2019-02-26 16:32:23 +0000

    PRMT-81: fix the name of the role to assume

--- terraformscripts/tf/codepipeline/datasources.tf
+++ terraformscripts/tf/codepipeline/datasources.tf
@@ -5,5 +5,5 @@ locals {
     assumed_role_components = ["${split(":", local.assumed_role)}"]
     role_name = "${local.assumed_role_components[5]}"
     role_name_components = ["${split("/", local.role_name)}"]
-    role = "${join(":", slice(local.assumed_role_components, 0, 5))}:role/${local.role_name_components[1]}"
+    role = "arn:aws:iam::${local.role_name_components[4]}:role/${local.role_name_components[1]}"
 }
