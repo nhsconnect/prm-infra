6f71a73c6800e66671d20bdfdb56e41b15fb068c
Dale Peakall <dale@peakall.net>
2019-02-26 16:49:02 +0000

    PRMT-81: pass the environment variables through to the updated infra test build

--- terraformscripts/tf/infratest/main.tf
+++ terraformscripts/tf/infratest/main.tf
@@ -1,5 +1,14 @@
 data "aws_caller_identity" "current" {}
 
+
+locals {
+    assumed_role = "${data.aws_caller_identity.current.arn}"
+    assumed_role_components = ["${split(":", local.assumed_role)}"]
+    role_name = "${local.assumed_role_components[5]}"
+    role_name_components = ["${split("/", local.role_name)}"]
+    role = "arn:aws:iam::${local.assumed_role_components[4]}:role/${local.role_name_components[1]}"
+}
+
 data "aws_iam_role" "codebuild_role" {
     name = "codebuild-project-generic-role"
 }
@@ -18,6 +27,21 @@ resource "aws_codebuild_project" "prm-servinginfra-lambdas-test" {
     compute_type = "BUILD_GENERAL1_SMALL"
     image        = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/codebuild/node:latest"
     type         = "LINUX_CONTAINER"
+
+    environment_variable {
+      name  = "ASSUME_ROLE_NAME"
+      value = "${local.role}"
+    }
+
+    environment_variable {
+      name = "ENVIRONMENT"
+      value = "${var.environment}"
+    }
+
+    environment_variable {
+      name = "ACCOUNT_ID"
+      value = "${data.aws_caller_identity.current.account_id}"
+    }
   }
 
   source {
