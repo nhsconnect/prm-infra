a0157a05becd54fbeaf035db6c732657c4c5d1a3
Phil Evans <phil@Phil-Evans-MacBook-Pro.local>
2019-10-10 15:49:35 +0100

    [PE] Added API Gateway Resource Policy

--- terraformscripts/tf/modules/apigw_endpoint/apigw_endpoint.tf
+++ terraformscripts/tf/modules/apigw_endpoint/apigw_endpoint.tf
@@ -1,6 +1,31 @@
 resource "aws_api_gateway_rest_api" "api_endpoint" {
   name        = "${var.environment}-${var.api_gateway_endpoint_name}"
   description = "${var.environment}-${var.api_gateway_endpoint_name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "execute-api:Invoke",
+            "Resource": "arn:aws:execute-api:eu-west-2:327778747031:aa826k16a9/*/*/*"
+        },
+        {
+            "Effect": "Deny",
+            "Principal": "*",
+            "Action": "execute-api:Invoke",
+            "Resource": "arn:aws:execute-api:eu-west-2:327778747031:aa826k16a9/*/*/*",
+            "Condition": {
+                "NotIpAddress": {
+                    "aws:SourceIp": ["194.101.83.23/32", "1.2.3.4/32"]
+                }
+            }
+        }
+    ]
+}
+EOF  
 }
 
 resource "aws_api_gateway_account" "acc" {
