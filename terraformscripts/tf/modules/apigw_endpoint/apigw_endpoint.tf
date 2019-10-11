d3b1130e994c9931a37b97ec14f1a33cd03445e3
Phil Evans <phil.evans@thoughtworks.com>
2019-10-11 13:50:59 +0100

    [PE] Added Office IP to API Gateway Resource Policy

--- terraformscripts/tf/modules/apigw_endpoint/apigw_endpoint.tf
+++ terraformscripts/tf/modules/apigw_endpoint/apigw_endpoint.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_rest_api" "api_endpoint" {
                     "aws:SourceIp": [
                         "194.101.83.23/32",
                         "35.176.92.32/29",
-                        "185.205.172.82"
+                        "185.205.172.82/32"
                     ]
                 }
             }
